8bc3dfb0a5d18f6b64f0d02728cc21df577a7829
firaga <zhou.jichen@gmail.com>
2023-10-29 17:51:15 +0800

    05 项目启动

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,21 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!--  <div>-->
+  <!--    <a href="https://vitejs.dev" target="_blank">-->
+  <!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+  <!--    </a>-->
+  <!--    <a href="https://vuejs.org/" target="_blank">-->
+  <!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+  <!--    </a>-->
+  <!--  </div>-->
+  <!--  <HelloWorld msg="你好，极客时间" />-->
+    <div>
+      <router-link to="/">首页</router-link>
+      |
+      <router-link to="/about">关于</router-link>
+    </div>
+    <router-view></router-view>
 </template>
 
 <style scoped>
@@ -21,9 +27,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
