9cff587138f5294c83d0111b52c75a7bc69e2911
firaga <zhou.jichen@gmail.com>
2023-10-29 18:26:25 +0800

    06

--- src/components/Todolist.vue
+++ src/components/Todolist.vue
@@ -14,38 +14,65 @@
       <span> {{ active }} / {{ all }} </span>
     </div>
   </div>
+  <div>
+    <h1 @click="add">{{ count }}</h1>
+  </div>
 </template>
 
 <script setup>
 import {ref, computed} from "vue";
 
-let title = ref("");
-let todos = ref([{title: '学习Vue', done: false}])
+let count = ref(1)
+let color = ref('red')
 
-function addTodo() {
-  todos.value.push({
-    title: title.value,
-    done: false,
-  });
-  title.value = "";
+function add() {
+  count.value++
+  color.value = Math.random() > 0.5 ? "blue" : "red"
 }
 
-function clear() {
-  todos.value = todos.value.filter((v) => !v.done);
+let {title, todos, addTodo, clear, active, all, allDone} = useTodos();
+</script>
+<style scoped>
+h1 {
+  color: v-bind(color);
 }
+</style>>
+<script>
+import {computed} from "vue";
+import {useMouse} from "../utils/mouse.js";
 
-let active = computed(() => {
-  return todos.value.filter((v) => !v.done).length;
-});
-let all = computed(() => todos.value.length);
-let allDone = computed({
-  get: function () {
-    return active.value === 0;
-  },
-  set: function (value) {
-    todos.value.forEach((todo) => {
-      todo.done = value;
+let {x, y} = useMouse()
+
+function useTodos() {
+  let title = ref("aaa");
+  let todos = ref([{title: "学习Vue", done: false}]);
+
+  function addTodo() {
+    todos.value.push({
+      title: title.value,
+      done: false,
     });
-  },
-});
+    title.value = "";
+  }
+
+  function clear() {
+    todos.value = todos.value.filter((v) => !v.done);
+  }
+
+  let active = computed(() => {
+    return todos.value.filter((v) => !v.done).length;
+  });
+  let all = computed(() => todos.value.length);
+  let allDone = computed({
+    get: function () {
+      return active.value === 0;
+    },
+    set: function (value) {
+      todos.value.forEach((todo) => {
+        todo.done = value;
+      });
+    },
+  });
+  return {title, todos, addTodo, clear, active, all, allDone};
+}
 </script>
---
+++ src/components/Todolist_bak.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script setup>
+import { ref } from "vue";
+let count = ref(1)
+let color = ref('red')
+function add() {
+  count.value++
+  color.value = Math.random()>0.5? "blue":"red"
+}
+</script>
+
+<style scoped>
+h1 {
+  color:v-bind(color);
+}
+</style>>
