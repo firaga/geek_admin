daa2d356a03d7630c1a01ac4bb12f65b118b2590
firaga <zhou.jichen@gmail.com>
2023-10-30 13:44:37 +0800

    elements引入

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,14 @@
 <template>
-  <h1>这是首页</h1>
+  <h1>这是首页1</h1>
   <Todolist/>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
 </template>
 <script setup>
 import Todolist from "../components/Todolist.vue";
